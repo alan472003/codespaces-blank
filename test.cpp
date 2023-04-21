bdf4b453686b8841622aee16c84e9a0da974295b
Alan Whitehouse <alan472003@outlook.com>
2023-04-21 21:54:20 +0000

    first commit

---
+++ test.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  cout << "Hello CodeSpaces!" << endl;
+
+  // write a program that prints out the numbers 1 to 100
+
+  for(int i = 1; i <= 100; i++) {
+    cout << i << ", ";
+  }
+  cout << endl;
+
+
+
+
+  return 0;
+}
